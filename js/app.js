926d2c3534dec63c76dbf687576beaade7456705
Niekon01 <adarsh90909090@gmail.com>
2020-04-27 19:27:42 +0530

    Added hindi and made mobile friendly

--- js/app.js
+++ js/app.js
@@ -67,6 +67,7 @@ fetch('https://api.covid19india.org/data.json')
 
     // Yesterday's Report
     latestDate = data.cases_time_series.length-1;
+    document.getElementById('date').innerHTML = "( " + data.cases_time_series[latestDate].date + ")";
     document.getElementById('yUntilCases').innerHTML = data.cases_time_series[latestDate].totalconfirmed;
     document.getElementById('yConfirmed').innerHTML = data.cases_time_series[latestDate].dailyconfirmed;
     document.getElementById('yRecovered').innerHTML = data.cases_time_series[latestDate].dailyrecovered;
